b74e7d89520d32e7db7e4caefcbfe1fc932392de
unknown <benjamin.borello@aol.com>
2022-12-02 05:17:35 +0100

    add signin & signup to sidebar

--- Application/template/components/side_bar.php
+++ Application/template/components/side_bar.php
@@ -23,6 +23,84 @@
         </div>
     </div>
 
+    <div class="menu sign">
+        <div>
+            <img src="../image/v2.png" alt="mepexa logo" style="border-radius: 999999px;">
+            <h3>Sign up</h3>
+        </div>
+        <form method="POST">
+            <table>
+                <tr>
+                    <td>
+                        <label for="username">Username</label>
+                    </td>
+                    <td>
+                        <input type="text" name="username" id="username" required>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="email">Email</label>
+                    </td>
+                    <td>
+                        <input type="email" name="email" id="email" required>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="password">Password</label>
+                    </td>
+                    <td>
+                        <input type="password" name="password" id="password" required>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="password_confirm">Confirm password</label>
+                    </td>
+                    <td>
+                        <input type="password" name="password_confirm" id="password_confirm" required>
+                    </td>
+                </tr>
+            </table>
+            <input class="button highlight_bt" type="submit" value="Sign-up">
+            <div class="button" type="submit">
+                <p>Back</p>
+            </div>
+        </form>
+    </div>
+
+    <div class="menu sign">
+        <div>
+            <img src="../image/v2.png" alt="mepexa logo" style="border-radius: 999999px;">
+            <h3>Sign in</h3>
+        </div>
+        <form method="POST">
+            <table>
+                <tr>
+                    <td>
+                        <label for="username">Username</label>
+                    </td>
+                    <td>
+                        <input type="text" name="username" id="username" required>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="password">Password</label>
+                    </td>
+                    <td>
+                        <input type="password" name="password" id="password" required>
+                    </td>
+                </tr>
+            </table>
+            <input class="button highlight_bt" type="submit" value="Sign-in">
+            <div class="button" type="submit">
+                <p>Back</p>
+            </div>
+        </form>
+    </div>
+
     <div id="second-menu" class="menu">
         <ul>
             <li><a href="">Report a problem</a></li>
